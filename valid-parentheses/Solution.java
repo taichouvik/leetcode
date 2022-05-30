348747b8e9aa77570af84be8f8a4f5ba00e078da
Vikalp Bhandari <97vikalp@gmail.com>
2022-05-30 14:20:25 -0400

    https://leetcode.com/problems/valid-parentheses/

---
+++ valid-parentheses/Solution.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+import java.util.stream.Stream;
+
+public class Solution{
+    public static boolean isValid(String s) {
+    Stack<String> stack = new Stack<>();
+    Map<String,String> map = new HashMap<>();
+
+    //mapping brackets
+    map.put("]","[");
+    map.put("}","{");
+    map.put(")","(");
+
+
+    for(int i=0;i<s.length();i++){
+        int ptr = i;
+        if( Stream.of("(","[","{").anyMatch(ii -> ii.equals(s.charAt(ptr)+"")))
+            stack.push(s.charAt(i)+""); //push opening brackets
+        else{
+            if(stack.empty()) // closing bracket but no brackets in stack
+                return false;
+           if(stack.peek().equals(map.get(s.charAt(i)+""))){
+                stack.pop(); //closing bracket matches opening brancket at head
+           }
+           else // closing bracket but no opening bracket
+            return false;
+        }
+    }
+    if(stack.empty() && s.length()>1) //string validated so stack empty
+        return true;
+    return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));
+    }
+}
