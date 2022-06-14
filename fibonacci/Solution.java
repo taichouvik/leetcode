d8976c146f1438dea88052d33bbb910bf5b95c76
Vikalp Bhandari <97vikalp@gmail.com>
2022-06-14 19:49:54 +0530

    https://leetcode.com/problems/fibonacci-number/

---
+++ fibonacci/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+  public static int fib(int n) {
+    int[] dp = new int[31];
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++)
+      dp[i] = dp[i - 1] + dp[i - 2];
+    return dp[n];
+  }
+
+  public static void main(String[] args) {
+      System.out.println(fib(6));
+  }
+}
