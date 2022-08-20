032adcf0179d15a45496ec72252dad262e9f4fe5
Vikalp Bhandari <97vikalp@gmail.com>
2022-08-20 14:38:03 +0530

    //https://leetcode.com/problems/all-possible-full-binary-trees/

--- dynamic-programming/range/all-possible-full-binary-trees/Solution.java
+++ dynamic-programming/range/all-possible-full-binary-trees/Solution.java
@@ -21,6 +21,8 @@ class TreeNode {
     }
 }
 
+//too hard required good observation and good coding
+//https://leetcode.com/problems/all-possible-full-binary-trees/
 public class Solution {
     // 1 2 3 4 5 6 7 nodes
     // choose a root among the nodes to create subproblems
@@ -35,25 +37,45 @@ public class Solution {
     // 1 2 3 nodes available
     // (1 2 3) subproblem can be used above for n=4
 
-    public static List<TreeNode> allPossibleFBT(int n) {
-        List<List<TreeNode>> dp = new ArrayList<>();
-        dp.add(Arrays.asList(new TreeNode(0)));
-        if (n == 2)
-            return Arrays.asList();
-        if (n == 1)
-            return dp.get(0);
-        // divide into odd left part and right part
-        for (int i = 1; i <= n; i++) {
-            if ((i & 1) == 0) {
-                dp.add(Arrays.asList(new TreeNode(0)));
-                continue;
+    List<List<TreeNode>> dp = new ArrayList<>();
+    public List<TreeNode> allPossibleFBT(int n) {
+        for(int i =0;i<=n;i++)
+            dp.add(new ArrayList<>());
+      return pick(n);
+    }
+    
+    List<TreeNode> pick(int n){
+        if(dp.get(n).size() != 0)
+            return dp.get(n);
+        if(n==1)
+            return Arrays.asList(new TreeNode(0));
+        if(n==2)
+            return new ArrayList<>();
+        List<TreeNode> subAns = new ArrayList<>();
+        for(int i = 1; i <= (n-1)/2; i++){
+            List<TreeNode> left = pick(i);
+            if(left != null){
+                List<TreeNode> right = pick(n-i-1);
+                if( right != null){
+                    for(TreeNode l : left){
+                        for(TreeNode r: right){
+                            subAns.add( new TreeNode(0,l, r));
+                            if(i != n-i-1)
+                               subAns.add( new TreeNode(0,r, l)); 
+                        }
+                    }
+                }
             }
-
-            for (int j = 1; j <= i; j += 2) {
-                int left = j-1;
-                int right = i-j;
+            else{
+                 List<TreeNode> right = pick(n-i-1);
+                if( right == null){
+                    subAns.add( new TreeNode(0));
+                }
             }
         }
+        if(! subAns.isEmpty())
+            dp.add(n, subAns);
+        return dp.get(n); 
     }
 
     public static void main(String[] args) {
